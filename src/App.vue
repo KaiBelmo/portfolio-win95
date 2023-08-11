1772c63b5743ff6ca0e4223c7eea2e941331ea83
KaiBelmo <belmomohamedali@gmail.com>
2023-08-11 17:17:13 +0200

    feat: add vueuse/head

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
     <desktop />
-</div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Desktop from './view/desktop.vue';
+import Desktop from "./view/desktop.vue";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "belmo's personal computer",
+  link: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+    },
+  ],
+  meta: [
+    { name: 'description', content: 'software engineer with expertise in both front-end and low-level programming.' },
+  ],
+});
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {createPinia} from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createHead } from "@vueuse/head"
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App).use(createHead()).use(createPinia()).mount('#app')
